4ea6090bc5294269fe707fc0d510a40ffbf45055
asamfellows <125497868+asamfellows@users.noreply.github.com>
2023-03-05 22:20:38 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ import datapane as dp
 import altair as alt
 from vega_datasets import data
 import matplotlib.pyplot as plt
-import chart_studio as py
+import chart_studio.plotly as py
 
 from PIL import Image
 import urllib
